a8daec52f9f23d968a30ac3ace2d9ac813c870d5
Yan Kawano <yankt2005@gmail.com>
2025-10-23 19:25:40 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/next.config.ts
+++ src/next.config.ts
@@ -9,9 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    allowedDevOrigins: ["**"],
-  },
   images: {
     remotePatterns: [
       {
